d26cdce6faf54e880ee43718b29e2844c62f47e8
dasha <darya.125viis.467@gmail.com>
2023-06-29 16:22:33 +0700

    adding a HashRouter

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,13 @@
 import { Pages } from 'pages/pages';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { setupStore } from 'store/store';
 import { Provider } from 'react-redux';
 import { ReactElement } from 'react';
-
-export const router = createBrowserRouter([{ path: '*', element: <Pages /> }]);
 
 const store = setupStore();
 
 export const App = (): ReactElement => (
     <Provider store={store}>
-        <RouterProvider router={router} />
+        <Pages />
     </Provider>
 );
 
--- src/pages/pages.tsx
+++ src/pages/pages.tsx
@@ -1,5 +1,5 @@
 import { Layout } from 'components/layout/layout';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import { AboutMe } from 'pages/aboutMe/aboutMe';
 import { ErrorPage } from 'pages/errorPage/errorPage';
 import { PostList } from 'pages/postList/postList';
@@ -7,36 +7,38 @@ import { User } from 'pages/user/user';
 import { ReactElement } from 'react';
 
 export const Pages = (): ReactElement => (
-    <Routes>
-        <Route path='/' element={<Layout />}>
-            <Route
-                path='/posts'
-                element={<PostList />}
-            />
-            <Route
-                path='/about-me'
-                element={<AboutMe />}
-            />
-            <Route
-                path='/user/:userId'
-                element={<User />}
-            />
-            <Route
-                path='/'
-                element={
-                    <Navigate replace to='/posts' />
-                }
-            />
-            <Route
-                path='/'
-                element={
-                    <Navigate replace to='/posts' />
-                }
-            />
-            <Route
-                path='*'
-                element={<ErrorPage />}
-            />
-        </Route>
-    </Routes>
+    <HashRouter>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route
+                    path='/posts'
+                    element={<PostList />}
+                />
+                <Route
+                    path='/about-me'
+                    element={<AboutMe />}
+                />
+                <Route
+                    path='/user/:userId'
+                    element={<User />}
+                />
+                <Route
+                    path='/'
+                    element={
+                        <Navigate replace to='/posts' />
+                    }
+                />
+                <Route
+                    path='/'
+                    element={
+                        <Navigate replace to='/posts' />
+                    }
+                />
+                <Route
+                    path='*'
+                    element={<ErrorPage />}
+                />
+            </Route>
+        </Routes>
+    </HashRouter>
 );
